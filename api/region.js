745836af202f4ccaf62b1b97b8632281f91b95d4
Iamdewale <dewalefams@gmail.com>
2025-08-08 12:19:28 +0100

    region api refactor

--- api/region.js
+++ api/region.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
   if (req.method === "OPTIONS") return res.status(200).end();
 
   try {
-    const API_URL = `https://app.beekeys.com/nigeria/wp-json/geodir/v2/locations/regions/?consumer_key=${process.env.BEEKEYS_KEY}&consumer_secret=${process.env.BEEKEYS_SECRET}`;
+    const API_URL = `https://app.beekeys.com/nigeria/wp-json/geodir/v2/locations/regions/?consumer_key=${process.env.NG_API_KEY}&consumer_secret=${process.env.NG_SECRET_KEY}`;
 
     const response = await fetch(API_URL);
     const contentType = response.headers.get("content-type");
@@ -32,7 +32,7 @@ module.exports = async (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json({
       message: "Success!",
-      data: json.data.slice(0, 3), // ✅ or remove slice to get all
+      data: json.data.slice(0, 3), // or all data
     });
   } catch (error) {
     console.error("API Proxy Error:", error.message);
